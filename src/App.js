0ad7dfb0b3cf01dd6bd1e4334470e7564d622c8d
Chandelier Axel <axelchandelier0@gmail.com>
2021-09-13 19:34:45 +0200

    feat: deploy

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Home from "./pages/Home";
-import Knowledges from "./pages/Knowledges";
-import Contact from "./pages/Contact";
-import Portfolio from "./pages/Portfolio";
-import NotFound from "./pages/NotFound";
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Knowledges from './pages/Knowledges';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import NotFound from './pages/NotFound';
 
 const App = () => {
-	return (
-		<>
-			<BrowserRouter>
-				<Switch>
-					<Route path="/" exact component={Home} />
-					<Route path="/compétences" component={Knowledges} />
-					<Route path="/portfolio" component={Portfolio} />
-					<Route path="/contact" component={Contact} />
-					<Route component={NotFound} />
-					{/* Ici on relie nos paths aux pages dédidées*/}
-				</Switch>
-			</BrowserRouter>
-		</>
-	);
+  return (
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/react-cv' exact component={Home} />
+          <Route path='/compétences' component={Knowledges} />
+          <Route path='/portfolio' component={Portfolio} />
+          <Route path='/contact' component={Contact} />
+          <Route component={NotFound} />
+          {/* Ici on relie nos paths aux pages dédidées*/}
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
